250ca16d6f058dd9a54aa07a60f227ca0c296090
Anato-Eini <jameskennethacabal@gmail.com>
2023-11-30 07:55:59 +0800

    Merge Sort

--- LSD Radix Sort/main.cpp
+++ LSD Radix Sort/main.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 int getMax(const vector<int>& arr) {
     int max = arr[0];
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] > max) {
+    for (int i = 1; i < arr.size(); i++)
+        if (arr[i] > max)
             max = arr[i];
-        }
-    }
     return max;
 }
 void radixSortLSD(vector<int>& arr) {
@@ -15,24 +13,19 @@ void radixSortLSD(vector<int>& arr) {
     for (int exp = 1; max / exp > 0; exp *= 10) {
         vector<int> output(arr.size());
         vector<int> count(10, 0);
-        for (int i : arr) {
+        for (int i : arr)
             count[(i / exp) % 10]++;
-        }
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 10; i++)
             count[i] += count[i - 1];
-        }
-        for (int i = arr.size() - 1; i >= 0; i--) {
+        for (int i = arr.size() - 1; i >= 0; i--)
             output[--count[(arr[i] / exp) % 10]] = arr[i];
-        }
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < arr.size(); i++)
             arr[i] = output[i];
-        }
     }
 }
 void printVector(const vector<int>& arr) {
-    for (int i : arr) {
+    for (int i : arr)
         cout << i << " ";
-    }
     cout << endl;
 }
 int main() {
