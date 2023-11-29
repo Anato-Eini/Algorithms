767204aa821e5398576ee02254d5b1f5a5d9c0a1
Anato-Eini <jameskennethacabal@gmail.com>
2023-11-30 07:57:12 +0800

    Merge Sort

---
+++ Merge Sort/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Function to merge two sorted subarrays
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays
+    for (i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    // Merge the temporary arrays back into arr[left..right]
+    i = 0;  // Initial index of first subarray
+    j = 0;  // Initial index of second subarray
+    k = left;  // Initial index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge Sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        // Same as (left + right) / 2, but avoids overflow for large left and right
+        int mid = left + (right - left) / 2;
+
+        // Recursively sort the first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int arr[size], b;
+    for(int a = 0; a < size; a++){
+        cout << "Enter element " << a + 1 << ": ";
+        cin >> arr[a];
+    }
+    cout << "Original array: ";
+    printArray(arr, size);
+    mergeSort(arr, 0, size - 1);
+    cout << "Sorted array: ";
+    printArray(arr, size);
+    return 0;
+}
