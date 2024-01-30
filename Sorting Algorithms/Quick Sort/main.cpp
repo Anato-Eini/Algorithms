2ff9c78d0366cf68bbdf48abdd69b9b3971a2311
Anato-Eini <jameskennethacabal@gmail.com>
2024-01-30 23:13:21 +0800

    Quick Sort

--- Sorting Algorithms/Quick Sort/main.cpp
+++ Sorting Algorithms/Quick Sort/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
-void quickSort(int array[], int size){
 
+int partition(int array[], int left, int right){
+    int pivot = array[right], low = left;
+
+    for(int i = left; i <= right; i++)
+        if(pivot > array[i])
+            swap(array[i], array[low++]);
+
+    swap(array[right], array[low]);
+    return low;
+}
+
+void quickSort(int array[], int left, int right){
+    if(left < right){
+        int partitionIndex = partition(array, left, right);
+
+        quickSort(array, left, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, right);
+    }
 }
 int main(){
     int size;
@@ -13,7 +30,7 @@ int main(){
     for(int i = 0; i < size; i++)
         cin >> array[i];
 
-    quickSort(array, size);
+    quickSort(array, 0, size - 1);
 
     cout << "Sorted array: ";
     for(int i = 0; i < size; i++)
