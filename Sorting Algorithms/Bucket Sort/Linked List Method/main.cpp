31b781304fa87685d27e35d29cb78364779f7db7
Destiny <jameskennethacabal@gmail.com>
2024-02-25 07:13:16 +0800

    Bucket Sort Linked List Method

---
+++ Sorting Algorithms/Bucket Sort/Linked List Method/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cassert>
+#define INTERVAL 10
+using namespace std;
+#include "BucketSort.h"
+int main(){
+    vector<int> arr;
+    int size, element;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    cout << "Enter the elements of the array: ";
+    for(int b = 1; b <= size; b++){
+        cin >> element;
+        arr.push_back(element);
+    }
+    cout << "Original array: ";
+    printVector(arr);
+    bucketSort(arr);
+    cout << "Sorted array: ";
+    printVector(arr);
+    return 0;
+}
