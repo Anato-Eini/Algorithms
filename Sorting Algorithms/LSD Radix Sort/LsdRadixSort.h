eb673dea2cafaa52cc63c2d443537b901d23a505
Anato-Eini <jameskennethacabal@gmail.com>
2024-02-25 21:34:08 +0800

    Sorting with negative numbers

--- Sorting Algorithms/LSD Radix Sort/LsdRadixSort.h
+++ Sorting Algorithms/LSD Radix Sort/LsdRadixSort.h
@@ -8,7 +8,20 @@ int getMax(const vector<int>& arr) {
             max = arr[i];
     return max;
 }
+
+int getMin(const vector<int>& arr){
+    int minimum = 0;
+    for(int i: arr)
+        if(minimum > i)
+            minimum = i;
+    return minimum;
+}
+
 void radixSortLSD(vector<int>& arr) {
+    int min = getMin(arr);
+    if(min < 0)
+        for (int & i: arr)
+            i -= min;
     int max = getMax(arr);
     for (int exp = 1; max / exp > 0; exp *= 10) {
         vector<int> output(arr.size());
@@ -17,10 +30,13 @@ void radixSortLSD(vector<int>& arr) {
             count[(i / exp) % 10]++;
         for (int i = 1; i < 10; i++)
             count[i] += count[i - 1];
-        for (int i = arr.size() - 1; i >= 0; i--)
+        for (int i = (int) arr.size() - 1; i >= 0; i--)
             output[--count[(arr[i] / exp) % 10]] = arr[i];
         arr = output;
     }
+    if(min < 0)
+        for(int & i: arr)
+            i += min;
 }
 void printVector(const vector<int>& arr) {
     for (int i : arr)
--- Sorting Algorithms/LSD Radix Sort/main.cpp
+++ Sorting Algorithms/LSD Radix Sort/main.cpp
@@ -9,10 +9,8 @@ int main() {
     cout << "Enter the size of the array: ";
     cin >> a;
     cout << "Enter the elements of the array: ";
-    for(int b = 1; b <= a; b++){
-        cin >> c;
-        arr.push_back(c);
-    }
+    for(int b = 1; b <= a; b++)
+        cin >> c, arr.push_back(c);
     cout << "Original array: ";
     printVector(arr);
     radixSortLSD(arr);
